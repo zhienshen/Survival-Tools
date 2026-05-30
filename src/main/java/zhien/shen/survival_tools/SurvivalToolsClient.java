5e656c6794d1b0aefbf8eb41ba5c4123339343c2
zhienshen <huan18971374646@163.com>
2026-05-30 15:29:17 +0800

    Initial commit

---
+++ src/main/java/zhien/shen/survival_tools/SurvivalToolsClient.java
@@ -0,0 +1,10 @@
+package zhien.shen.survival_tools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurvivalToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/zhien/shen/survival_tools/SurvivalToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package zhien.shen.survival_tools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SurvivalToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
